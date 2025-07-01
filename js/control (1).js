9d77ac266eb29d6fad1965ee90fea197fdb51fa7
yue9213-rrbg <nguyenthiminhngoc403@gmail.com>
2025-07-01 17:43:25 +0700

    Add files via upload

---
+++ js/control (1).js
@@ -0,0 +1,11 @@
+
+var typed = new Typed('.element', {
+strings: ["UI/UX Designer", "Content Creator","Video Editor"],
+typeSpeed:50,
+backSpeed:50,
+loop:true,
+});
+// JavaScript Document
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
